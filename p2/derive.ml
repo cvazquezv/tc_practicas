c181bbbe99de00bc00177efd943cb9e95b3aa69e
cvazquezv <c.vazquezv@udc.es>
2026-03-14 19:17:52 +0100

    creado derive.ml

---
+++ p2/derive.ml
@@ -0,0 +1,18 @@
+(* EJERCICIO 2 *)
+
+(*val regexp_of_string : string -> Regexp.regexp*)
+(*construye valor tipo regexp a partir de un string con una expresión regular
+en formato Posix utilizando el parser*)
+
+(*val nullable         : Regexp.regexp -> Regexp.regexp*)
+(*para una expresión regular r devuelva v(r)*)
+
+(*val derive           : char -> Regexp.regexp -> Regexp.regexp*)
+(*para un carácter c y una expresión regular r devuelva  ∂c(r)*)
+
+(*val matches_regexp   : string -> Regexp.regexp -> bool*)
+(*calcula si un string encaja con una expresión regular*)
+
+(*val matches          : string -> string -> bool*)
+(*donde matches str1 str2 calcula si str1 encaja con la expresióón
+regular en formato Posix de str2*)

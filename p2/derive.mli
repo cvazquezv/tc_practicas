9d99646ec8cb45ae0c132f2d77a366fe873c4296
Cristina Vázquez Varela <c.vazquezv@udc.es>
2026-03-30 13:02:21 +0200

    Delete p2 directory

--- p2/derive.mli
+++
@@ -1,6 +0,0 @@
-val regexp_of_string : string -> Regexp.regexp
-val nullable         : Regexp.regexp -> Regexp.regexp
-val derive           : char -> Regexp.regexp -> Regexp.regexp
-val matches_regexp   : string -> Regexp.regexp -> bool
-val matches          : string -> string -> bool
-val simplify : Regexp.regexp-> Regexp.regexp

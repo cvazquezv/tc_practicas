c5f2e0fee158dd533180cea929d1ad2841327f9f
cvazquezv <c.vazquezv@udc.es>
2026-04-30 17:34:36 +0200

    definido el tipo para la gramática

--- p3/cyk.ml
+++ p3/cyk.ml
@@ -1,5 +1,21 @@
+(*TIPOS DE SÍMBOLOS EN LA GRAMÁTICA*)
+
+type symbol = T of char | NT of char
+
+(*T : Terminal (minúsculas)*)
+(*NT : No Terminal (mayúsculas)*)
+
+type rule = { (*ejemplo del pdf: S->AB*)
+    left: symbol; (*parte izquierda de la regla: S*)
+    right: symbol list (*parte derecha de la regla: AB*)
+}
+
+type grammar = {
+    start: symbol; (*símbolo inicial de la gramática*)
+    rules: rule list (*lista de reglas de la gramática*)
+}
+
 (*==================Ejercicio 1=====================*)
-type tree = Node of string * tree list
 
 
 (*==================Ejercicio 2=====================*)
